f2377f71a35b7a6d4d063a4cc7a2aa6492f7b0a3
laura7164 <164506063+laura7164@users.noreply.github.com>
2024-09-26 19:49:48 -0300

    Add files via upload

--- gerenciador de livraria/main.py
+++ gerenciador de livraria/main.py
@@ -2,33 +2,39 @@ import sqlite3
 
 def exibir_menu():
     print()
-    print('1. Adicionar novo livro')
-    print('2. Exibir todos os livros')
-    print('3. Atualizar preço de um livro')
+    print('1. Adicionar um novo livro')
+    print('2. Exibir todos os livros cadastrados')
+    print('3. Atualizar o preço de um livro')
     print('4. Remover um livro')
-    print('5. Sair')
+    print('5. Buscar livros por autor')
+    print('6. Exportar dados para o CSV')
+    print('7. Importar dados do CSV')
+    print('8. Fazer backup do banco de dados')
+    print('9. Sair')
 
+# conexão com o banco de dados SQLite
 conexao = sqlite3.connect('minha_livraria.db') 
 cursor = conexao.cursor()
 
+# criação da tabela se não existir
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS livraria (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         titulo TEXT NOT NULL,
         autor TEXT NOT NULL,
-        ano_publicacao TEXT NOT NULL,
+        ano_publicacao INTEGER NOT NULL,
         preco FLOAT NOT NULL
     )
 ''')
 
-def adicionar_livro(titulo: str, autor: str, ano_publicacao: str, preco: float):
+def adicionar_livro(titulo: str, autor: str, ano_publicacao: int, preco: float):
     cursor.execute('INSERT INTO livraria (titulo, autor, ano_publicacao, preco) VALUES (?, ?, ?, ?)', (titulo, autor, ano_publicacao, preco))
 
 def exibir_livros():
     cursor.execute('SELECT * FROM livraria')
-    livraria = cursor.fetchall()
+    resultado = cursor.fetchall()
 
-    for livro in livraria:
+    for livro in resultado:
         print(livro)
 
 def atualizar_preco(preco: float, titulo: str):
@@ -37,44 +43,89 @@ def atualizar_preco(preco: float, titulo: str):
 def remover_livro(titulo: str):
     cursor.execute('DELETE FROM livraria WHERE titulo = ?', (titulo,))
 
+def buscar_livro(autor: str):
+    cursor.execute('SELECT * FROM livraria WHERE autor = ?', (autor))
+    resultado = cursor.fetchall()
+
+    for livro in resultado:
+        print(livro)
+
 while True:
     exibir_menu()
 
-    opcao = int(input('Escolha uma opção: '))
+    try:
+        opcao = int(input('Escolha uma opção: '))
+    except ValueError:
+        print('Por favor, insira um número válido.')
+        continue
 
     if opcao == 1:
         titulo = input('Digite o titulo do livro: ')
         autor = input(f'Digite o autor do livro "{titulo}": ')
-        ano_publicacao = input(f'Digite o ano de publicação do livro "{titulo}": ')
-        preco = float(input(f'Digite o preço do livro "{titulo}": '))
+
+        while True:
+            try:
+                ano_publicacao = int(input(f'Digite o ano de publicação do livro "{titulo}": '))
+                break
+            except ValueError:
+                print('Por favor, insira apenas números.')
+
+        while True:
+            try:
+                preco = float(input(f'Digite o preço do livro "{titulo}": '))
+                break
+            except ValueError:
+                print('Por favor, insira um valor numérico para o preço.')
+
 
         adicionar_livro(titulo, autor, ano_publicacao, preco)
-
-        print('Livro cadastrado com sucesso!')
-        
+        print(f'{titulo} cadastrado com sucesso!')
+ 
     elif opcao == 2:
         exibir_livros()
 
     elif opcao == 3:
         titulo = input('Digite o livro que deseja alterar o preço: ')
-        novo_preco = float(input(f'Digite o novo preço do livro "{titulo}": '))
 
-        print('Preço atualizado com sucesso!')
+        while True:
+            try:
+                novo_preco = float(input(f'Digite o novo preço do livro "{titulo}": '))
+                break
+            except ValueError:
+                print('Por favor, insira um valor numérico para o preço.')
 
         atualizar_preco(novo_preco, titulo)
+        print('Preço atualizado com sucesso!')
 
     elif opcao == 4:
         titulo = input('Digite o livro que deseja remover: ')
 
-        print('Livro removido com sucesso!')
-
         remover_livro()
+        print(f'{titulo} removido com sucesso!')
 
     elif opcao == 5:
-        print('Saindo.')
+        autor = input('Digite o nome do autor do livro que deseja buscar: ')
+
+        buscar_livro(autor)
+    
+    elif opcao == 6:
+        ...
+
+    elif opcao == 7:
+        ...
+    
+    elif opcao == 8:
+        ...
+    
+    elif opcao == 9:
+        print('Saindo do sistema...')
         break
 
     else:
         print('Opção inválida, digite novamente.')
 
+    # commit das alterações no banco de dados
     conexao.commit()
+
+# fechar a conexão após sair do loop
+conexao.close()
